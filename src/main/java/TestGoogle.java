d610f5c40b5e7bca1bcdf47ea694e8077cf7a614
Marcelo de Oliveira Santos <>
2022-11-03 18:20:29 -0300

    configuração inicial do projeto

---
+++ src/main/java/TestGoogle.java
@@ -0,0 +1,16 @@
+import org.junit.Assert;
+import org.junit.Ignore;
+import org.junit.Test;
+
+
+
+public class TestGoogle {
+
+
+	@Test
+	@Ignore
+	public void TestZero() {
+		
+		Assert.fail();
+	}
+}
